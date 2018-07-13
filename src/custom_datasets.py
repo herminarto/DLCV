f5d537714b2d8586469d6c792c70b4f922dafa23
herminarto <herminarto@yahoo.com>
2018-07-13 13:50:47 +0700

    Add code for dataset splitter

--- src/custom_datasets.py
+++ src/custom_datasets.py
@@ -10,10 +10,13 @@ import numpy as np
 from PIL import Image
 
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
 from torchvision import transforms
 from torch.utils.data.dataset import Dataset  # For custom datasets
+from torch.utils.data.sampler import SubsetRandomSampler
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+log_interval = 100
 
 class CustomDatasetFromImages(Dataset):
     def __init__(self, csv_path):
@@ -41,18 +44,8 @@ class CustomDatasetFromImages(Dataset):
         single_image_name = self.image_arr[index]
         # Open image
         img_as_img = Image.open(single_image_name)
-
-        # Check if there is an operation
-        # some_operation = self.operation_arr[index]
-        # If there is an operation
-#        if some_operation:
-#            # Do some operation on image
-#            # ...
-#            # ...
-#            pass
         # Transform image to tensor
         img_as_tensor = self.to_tensor(img_as_img)
-
         # Get label(class) of the image based on the cropped pandas column
         single_image_label = self.label_arr[index]
 
@@ -61,11 +54,32 @@ class CustomDatasetFromImages(Dataset):
     def __len__(self):
         return self.data_len
     
-transformations = transforms.Compose([transforms.ToTensor()])
+#transformations = transforms.Compose([transforms.ToTensor()])
 
-fire_datasets =  \
-        CustomDatasetFromImages('../data/fire_labels.csv')
+#fire_datasets =  CustomDatasetFromImages('../data/fire_labels.csv')
         
-fire_dataset_loader = torch.utils.data.DataLoader(dataset=fire_datasets,
-                                                    batch_size=10,
-                                                    shuffle=False)
+#fire_dataset_loader = torch.utils.data.DataLoader(dataset=fire_datasets,
+#                                                    batch_size=100,
+#                                                    shuffle=False)
+
+def CustomSplitLoader(datasets, batch_size, train_percentage, test_percentage, valid_percentage):
+    num_train = len(datasets)
+    indices = list(range(num_train))
+    split_test = int(np.floor(train_percentage/100 * num_train))
+    split_valid = int(np.floor(valid_percentage/100 * num_train))
+    train_indices, test_idx = indices[split_test:], indices[:split_test]
+    train_idx, valid_idx = train_indices[split_valid:], train_indices[:split_valid]
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+    
+    # create train, validation and test loader
+    train_loader = torch.utils.data.DataLoader(datasets, batch_size,
+                    sampler=train_sampler, num_workers=4, pin_memory=False,)
+    test_loader = torch.utils.data.DataLoader(datasets, batch_size,
+                    sampler=test_sampler, num_workers=4, pin_memory=False,)
+    valid_loader = torch.utils.data.DataLoader(datasets, batch_size,
+                    sampler=valid_sampler, num_workers=4, pin_memory=False,)
+    
+    return train_loader, test_loader, valid_loader
+    
